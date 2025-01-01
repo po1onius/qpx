e0125167b4af2f851c96cdda862ce54fdc4ca861
srus <po1onius@outlook.com>
2025-01-02 00:48:06 +0800

    generate map item from level data

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@ use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
 use serde::Deserialize;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::path::Path;
 
 fn main() -> AppExit {
     App::new()
         .insert_resource(LevelData::from_file("level_data/1.toml"))
-        .insert_resource(LevelIndex(0))
+        .insert_resource(IdxEntityPair::default())
         .add_plugins(DefaultPlugins)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_plugins(RapierDebugRenderPlugin::default())
@@ -42,6 +43,7 @@ struct MapItemBundle {
     collider: Collider,
     position: Transform,
     map_item: MapItem,
+    index: LevelIndex,
 }
 
 #[derive(Component)]
@@ -65,8 +67,13 @@ struct LevelData {
     data: Vec<MapItemData>,
 }
 
-#[derive(Resource)]
+#[derive(Component)]
 struct LevelIndex(u32);
+
+#[derive(Resource, Default)]
+struct IdxEntityPair {
+    pairs: HashMap<u32, (u32, Option<u32>)>,
+}
 
 const FLOOR_H: f32 = 5.0;
 const JUMP_SPEED: f32 = 600.0;
@@ -94,7 +101,7 @@ impl LevelData {
 }
 
 impl MapItemBundle {
-    fn rect_item(rect: Vec4, obstacle: bool) -> Self {
+    fn rect_item(rect: &Vec4, obstacle: bool, index: u32) -> Self {
         Self {
             rigid: RigidBody::Fixed,
             collider: Collider::cuboid(rect.z, rect.w - FLOOR_H),
@@ -104,39 +111,72 @@ impl MapItemBundle {
             } else {
                 MapItem::Normal
             },
+            index: LevelIndex(index),
         }
     }
 
-    fn tri_obstacle(tri: Triangle2d) -> Self {
+    fn tri_obstacle(tri: &Triangle2d, index: u32) -> Self {
         Self {
             rigid: RigidBody::Fixed,
             collider: Collider::triangle(tri.vertices[0], tri.vertices[1], tri.vertices[2]),
             position: Transform::from_xyz(tri.vertices[0].x, tri.vertices[1].y, 0.0),
             map_item: MapItem::Obstacle,
+            index: LevelIndex(index),
         }
     }
 }
 
-fn spawn_floor(cmd: &mut Commands, rect: Vec4) {
+fn spawn_floor(
+    cmd: &mut Commands,
+    rect: &Vec4,
+    index: u32,
+    lv_idx_entity_paires: &mut ResMut<IdxEntityPair>,
+) {
     let hw = 10.0;
     let hy = rect.y + rect.w - hw;
-    let floor_high = MapItemBundle::rect_item(Vec4::new(rect.x, hy, rect.z, hw), false);
-    let floor_low = MapItemBundle::rect_item(Vec4::new(rect.x, rect.y, rect.z, rect.w - hw), true);
-    cmd.spawn(floor_high);
-    cmd.spawn(floor_low);
+    let floor_high = MapItemBundle::rect_item(&Vec4::new(rect.x, hy, rect.z, hw), false, index);
+    let floor_low =
+        MapItemBundle::rect_item(&Vec4::new(rect.x, rect.y, rect.z, rect.w - hw), true, index);
+    let id1 = cmd.spawn(floor_high).id().index();
+    let id2 = cmd.spawn(floor_low).id().index();
+
+    lv_idx_entity_paires.pairs.insert(index, (id1, Some(id2)));
+    info!("spawn: index {}, entity {} {}", index, id1, id2);
 }
 
-fn setup(mut cmd: Commands, asset_server: Res<AssetServer>, level_data: Res<LevelData>) {
+fn spawn_tri_obstacle(
+    cmd: &mut Commands,
+    tri: &Triangle2d,
+    index: u32,
+    lv_idx_entity_paires: &mut ResMut<IdxEntityPair>,
+) {
+    let id = cmd
+        .spawn(MapItemBundle::tri_obstacle(tri, index))
+        .id()
+        .index();
+    lv_idx_entity_paires.pairs.insert(index, (id, None));
+    info!("sapwn: index {}, entity {}", index, id);
+}
+
+fn setup(
+    mut cmd: Commands,
+    asset_server: Res<AssetServer>,
+    level_data: Res<LevelData>,
+    mut lv_idx_entity_paires: ResMut<IdxEntityPair>,
+) {
     cmd.spawn(Camera2d::default());
     //let block_texture = asset_server.load("block.png");
 
-    for l in level_data.data.iter() {
+    for (i, l) in level_data.data.iter().enumerate() {
+        if i > 1 {
+            break;
+        }
         match l {
             MapItemData::Rect(rect) => {
-                spawn_floor(&mut cmd, *rect);
+                spawn_floor(&mut cmd, rect, i as u32, &mut lv_idx_entity_paires);
             }
             MapItemData::Tri(tri) => {
-                cmd.spawn(MapItemBundle::tri_obstacle(*tri));
+                spawn_tri_obstacle(&mut cmd, tri, i as u32, &mut lv_idx_entity_paires);
             }
         }
     }
@@ -205,13 +245,56 @@ fn role_move(
 fn loop_block(
     mut cmd: Commands,
     camera_transform: Single<&Transform, (With<Camera>, Without<RoleSpeed>, Without<MapItem>)>,
-    map_items: Query<(Entity, &mut Transform), With<MapItem>>,
+    level_data: Res<LevelData>,
+    mut lv_idx_entity_paires: ResMut<IdxEntityPair>,
+    map_item_entities: Query<(Entity, &MapItem)>,
 ) {
-    for (item_entity, item_transform) in map_items.iter() {
-        let far = camera_transform.translation.x - item_transform.translation.x;
-        if far > 500.0 {
-            //cmd.entity(item_entity).despawn();
-            //cmd.spawn(BlockBundle::default());
+    let entities: Vec<u32> = map_item_entities.iter().map(|(e, _)| e.index()).collect();
+    fn despawn_by_lv_idx(
+        cmd: &mut Commands,
+        lv_idx_entity_paires: &mut ResMut<IdxEntityPair>,
+        idx: u32,
+        entities: &Vec<u32>,
+    ) {
+        if let Some((entity_id1, entity_op_id2)) = lv_idx_entity_paires.pairs.get(&idx) {
+            if !entities.contains(entity_id1) {
+                return;
+            }
+            let entity1 = Entity::from_raw(*entity_id1);
+            info!("despawn: index {}, entity {}", idx, entity_id1);
+            cmd.entity(entity1).despawn();
+            if let Some(entity_id2) = entity_op_id2 {
+                if entities.contains(entity_id2) {
+                    let entity2 = Entity::from_raw(*entity_id2);
+                    info!("despawn: index {}, entity {}", idx, entity_id2);
+                    cmd.entity(entity2).despawn();
+                }
+            }
+        }
+        lv_idx_entity_paires.pairs.remove(&idx);
+    }
+    for (i, item_data) in level_data.data.iter().enumerate() {
+        match item_data {
+            MapItemData::Rect(rect) => {
+                if camera_transform.translation.x - (rect.x + rect.z) > 500.0 {
+                    despawn_by_lv_idx(&mut cmd, &mut lv_idx_entity_paires, i as u32, &entities);
+                }
+                let ng = rect.x - camera_transform.translation.x;
+                if ng < 500.0 && ng > 300.0 && !lv_idx_entity_paires.pairs.contains_key(&(i as u32))
+                {
+                    spawn_floor(&mut cmd, rect, i as u32, &mut lv_idx_entity_paires);
+                }
+            }
+            MapItemData::Tri(tri) => {
+                if camera_transform.translation.x - tri.vertices[2][0] > 500.0 {
+                    despawn_by_lv_idx(&mut cmd, &mut lv_idx_entity_paires, i as u32, &entities);
+                }
+                let ng = tri.vertices[0][0] - camera_transform.translation.x;
+                if ng < 500.0 && ng > 300.0 && !lv_idx_entity_paires.pairs.contains_key(&(i as u32))
+                {
+                    spawn_tri_obstacle(&mut cmd, tri, i as u32, &mut lv_idx_entity_paires);
+                }
+            }
         }
     }
 }
